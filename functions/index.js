dbc44d9d33821addf80379252dc87869249f2275
Johnny <47119111+lostmypillow@users.noreply.github.com>
2024-10-09 02:57:59 +0800

    added all relevant functions

--- functions/index.js
+++ functions/index.js
@@ -2,7 +2,7 @@
 import { onRequest } from "firebase-functions/v2/https";
 import { initializeApp } from "firebase-admin/app";
 import { FieldValue, getFirestore } from "firebase-admin/firestore";
-import { dmsScrape } from "dms-scrape";
+import {dmsScrape} from 'dms-scrape'
 import { send2LINE } from "./send2Line.mjs";
 import { getUTCDate } from "./getUTCDate.mjs";
 import { v4 as uuid } from "uuid";
@@ -24,32 +24,31 @@ const getTargetID = async (category, priority) => {
     console.log("No matching documents.");
     return;
   } else {
-    let eyed
-    snapshot.forEach(doc => {
+    let eyed;
+    snapshot.forEach((doc) => {
       if (doc.id !== undefined) {
-        eyed = doc.id
-      };
-     
-    }) 
-    return eyed
-    // return snapshot[0].id;
+        eyed = doc.id;
+      }
+    });
+    return eyed;
   }
 };
 export const addlineurl = onRequest(
   { cors: true, region: "asia-east1" },
   async (req, res) => {
     const data = await dmsScrape("link", req.body.events[0].message.text);
-    console.log(data)
+    console.log(data);
     data["id"] = uuid();
-    data["priority"] = data.category ? await getCountFromCategory(data.category) : 0
+    data["priority"] = data.category
+      ? await getCountFromCategory(data.category)
+      : 0;
     await getDocRef(data.id).set(data);
     // send2LINE(data.id, req.body.events[0].replyToken);
     res.send("Document ID " + data.id + " added");
   }
 );
-
 //{url: https://, html: `efnfkwnfkwnk`, id: 123}
-export const addexthtml = onRequest(
+export const addhtml = onRequest(
   { cors: true, region: "asia-east1" },
   async (req, res) => {
     const data = await dmsScrape("html", req.body.url, req.body.html);
@@ -60,8 +59,8 @@ export const addexthtml = onRequest(
       html: FieldValue.delete(),
       priority: await getCountFromCategory(data.category),
     };
-    console.log("updated data:")
-console.log(updateData)
+    console.log("updated data:");
+    console.log(updateData);
     await getDocRef(req.body.id).update(updateData);
 
     res.send("Document ID " + req.body.id + " modified");
@@ -72,27 +71,74 @@ console.log(updateData)
 export const update = onRequest(
   { cors: true, region: "asia-east1" },
   async (req, res) => {
-    await getDocRef(req.body.id).update(req.body);
+    if (req.query.edit == "data") {
+      let originalData = req.body;
+      const originalCategory = (await getDocRef(req.query.id).get()).data()
+        .category;
+      console.log("original category:" + originalCategory);
+      const originalPriority = (await getDocRef(req.query.id).get()).data()
+        .priority;
+      console.log("original priority:" + originalPriority);
+
+      if (originalCategory != req.body.category) {
+        originalData["priority"] = await getCountFromCategory(
+          req.body.category
+        );
+        const snapshot = await getColRef()
+          .where("category", "==", originalCategory)
+          .where("priority", ">", originalPriority)
+          .get();
+        if (snapshot.empty) {
+          console.log("No matching documents.");
+        } else {
+          let arr = [];
+          snapshot.forEach((doc) => {
+            arr.push(doc.data());
+          });
+          for (const doc of arr) {
+            await getDocRef(doc.id).update({
+              priority: FieldValue.increment(-1),
+            });
+          }
+        }
+      }
+      await getDocRef(req.query.id).update(originalData);
+    } else if (req.query.edit == "priority") {
+      await getDocRef(
+        await getTargetID(req.body.sourceCategory, req.body.targetPriority)
+      ).update({
+        priority: req.body.sourcePriority,
+      });
+      await getDocRef(req.body.sourceID).update({
+        priority: req.body.targetPriority,
+      });
+    } else if (req.query.edit == "select") {
+      await getDocRef(req.query.id).update(req.body);
+    } else if (req.query.edit == "unselect") {
+      await getDocRef(req.query.id).update({
+        selected_content: FieldValue.delete(),
+      });
+    }
+
+    res.send("Document ID " + req.query.id + " modified " + req.query.edit);
+  }
+);
+
+export const deleteDoc = onRequest(
+  { cors: true, region: "asia-east1" },
+  async (req, res) => {
+    await getDocRef(req.query.id).delete();
+    res.send("Document ID " + req.query.id + " deleted");
   }
 );
 // {"sourceID": , "sourceCategory": , "sourcePriority": , "targetPriority": }
-export const priority = onRequest(
-  { cors: true, region: "asia-east1" },
-  async (req, res) => {
-    // console.log(req.body)
-    // console.log(
-      
-    // );
-    await getDocRef(
-    await getTargetID(req.body.sourceCategory, req.body.targetPriority)
-    ).update({
-      priority: req.body.sourcePriority,
-    });
-    await getDocRef(req.body.sourceID).update({
-      priority: req.body.targetPriority,
-    });
-  }
-);
+// export const priority = onRequest(
+//   { cors: true, region: "asia-east1" },
+//   async (req, res) => {
+
+//     res.send("Document ID " + req.body.sourceID + " modified");
+//   }
+// );
 // {data: {title: "fef"}}
 // export const addextdata = onRequest(
 //   { cors: true, region: "asia-east1" },
